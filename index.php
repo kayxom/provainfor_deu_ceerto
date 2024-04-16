435e93bdec028a819d1f48b9b7948fba9706a7bc
kayxom <143403330+kayxom@users.noreply.github.com>
2024-04-16 20:40:46 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+
+    <div class="conteiner">
+        <div class="row">
+ <div class="col">
+
+
+<form>
+  
+ <div class="jumbotron">
+  <h1 class="display-4">inicio</h1>
+  <p class="lead">teste da aula do professor Sergio </p>
+  <hr class="my-4">
+  <p>site desenvolvidos pelos os alunos no 2º ano do ensino medio do curso tecnico de informatica da escola paula boarbosa leite</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">cadastra-se</a>
+    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">pesquisar</a>
+  </p>
+</div>
+
+
+
+
+
+</form>
+    
+  
+
+ </div>
+
+    </div>
+
+</div>
+    <div>
+    
