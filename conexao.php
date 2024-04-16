74ca2d3f994649600c26609507ebe0c4610a229c
kayxom <143403330+kayxom@users.noreply.github.com>
2024-04-16 20:41:34 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,17 @@
+<?php
+$host ="localhost";
+$user ="root";
+$senha="";
+$banco="cadastro";
+
+$conexao = new mysqli($host,$user,$senha,$banco);
+     if(!$conexao){
+        echo 'de errado'.mysqli_connect_error();
+    //echo'de errado'.mysqli_connect_error();
+}
+else{
+    //echo 'deu certo';
+    echo 'deu certo';
+}
+
+?>
