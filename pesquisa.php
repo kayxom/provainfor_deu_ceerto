70fe2a1cf4d093e31be895de658c80b95135a575
kayxom <143403330+kayxom@users.noreply.github.com>
2024-04-16 20:42:58 -0300

    Create pesquisa.php

---
+++ pesquisa.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+
+    <div class="conteiner">
+        <div class="row">
+ <div class="col">
+
+ <nav class="navbar navbar-light bg-light"action="pesquisa.php"method="post">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search" name="buscar">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+  
+</nav>
+       
+
+    </div>
+   </div>
+    </div>
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
