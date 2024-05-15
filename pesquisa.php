9066d300edf84def20fbe7179f082c9250e19ccb
kayxom <143403330+kayxom@users.noreply.github.com>
2024-05-15 08:22:35 -0300

    Update pesquisa.php

--- pesquisa.php
+++ pesquisa.php
@@ -7,43 +7,31 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" >
   </head>
   <body>
-
     <div class="conteiner">
-        <div class="row">
- <div class="col">
-
- <nav class="navbar navbar-light bg-light"action="pesquisa.php"method="post">
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search" name="buscar">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-  
+      <div class="row">
+        <div class="col">
+          <h1>pesquisar</h1>
+          <nav class="navbar navbar-light bg-light">
+  <form class="form-inline" method="post" action="pesquisar.php">
+    <input class="form-control mr-sm-2" type="search" placeholder="buscar " aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">pesquisa</button>
+  </form>
 </nav>
-       
-
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Email</th>
+      <th scope="col">Açoes </th>
+    
+    
+    </tr>
+  </thead>
+  
+</table>
+        </div>
+      </div>
     </div>
-   </div>
-    </div>
-    
-    
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
   </body>
 </html>
