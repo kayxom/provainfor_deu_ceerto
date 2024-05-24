ee80405bb0bbacef60480ddf88cf8f3c0c590b3e
kayxom <143403330+kayxom@users.noreply.github.com>
2024-05-24 16:51:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,42 @@
+
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet" >
-  </head>
-  <body>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
 
-    <div class="conteiner">
+<body>
+    <div class="container">
         <div class="row">
- <div class="col">
-
-
-<form>
-  
- <div class="jumbotron">
-  <h1 class="display-4">inicio</h1>
-  <p class="lead">teste da aula do professor Sergio </p>
-  <hr class="my-4">
-  <p>site desenvolvidos pelos os alunos no 2º ano do ensino medio do curso tecnico de informatica da escola paula boarbosa leite</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">cadastra-se</a>
-    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">pesquisar</a>
-  </p>
-</div>
-
-
-
-
-
-</form>
-    
-  
-
- </div>
-
+            <div class="col">
+                <div class="jumbotron">
+                    <h1 class="display-4">Sistema 2 c</h1>
+                    <p class="lead">Sistema desenvolvido por alunos do curso técnico em Informática.</p>
+                    <hr class="my-4">
+                    <p>EEEP PAULO BARBOSA LEITE .</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastre-se</a>
+                        <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisar</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 
-</div>
-    <div>
-    
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
