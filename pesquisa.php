5fa482841adbe9afac605ff8cf6f781d3fcb79f3
kayxom <143403330+kayxom@users.noreply.github.com>
2024-05-24 16:50:37 -0300

    Update pesquisa.php

--- pesquisa.php
+++ pesquisa.php
@@ -1,81 +1,104 @@
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>cadastrado</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
 <body>
-
-    <?php
-    $pesquisa= $_POST['busca']??'';
-    include"conexao.php";
-    $sql = "SELECT * FROM cadas where nome LIKE '%$pesquisa%'";//selecione toda a tabela cadas onde a coluna nome tenha um LIKE que filtre todas as letras que o usuário digita que esteja no inicio, no meio e no fim  
-    //em outras aplavras o $sql define a consulta  para selecionar registros da tabela cadas onde a coluna nome contém o termo de pesquisa.
-    
-    $dados = mysqli_query($conexao,$sql);//variavel que fazerá a consulta
+    <?php 
+    include "conexao.php";
+    $pesquisa = $_POST['busca'] ?? '';
+    $sql = "SELECT * FROM usuario WHERE nome LIKE'%$pesquisa%'";
+    $dados = mysqli_query($conexao,$sql);
     ?>
-
+    
     <div class="container">
         <div class="row">
             <div class="col">
                 <h1>Pesquisar</h1>
-                <nav class="navbar navbar-light bg-light">
-                    <form class="form-inline" method="post" action="pesquisa.php">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Digite a sua busca" aria-label="Search" name="busca">
-                        <br>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                    </form>
-                </nav>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Ações</th>
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <form class="d-flex" role="search" action="pesquisa.php" method="POST">
+                            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" name="busca">
+                            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+                        </form>
+                    </div>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            while($linha = mysqli_fetch_assoc($dados)){
+                                $id = $linha['id'];
+                                $nome = $linha['nome'];
+                                $email = $linha['email'];
 
+                                echo "<tr>
+                                
+                                <td>$nome</td>
+                                <td>$email</td>
+                                <td width=150px>
+                               <a href='editar.php? id=$id' class='btn btn-success btn-sm'>Editar</a>
+                               <a href='#' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#confirma'>Excluir</a>
+                               </td>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        while ($linha = mysqli_fetch_assoc($dados)) {//Utiliza um loop while para iterar pelos resultados da consulta e exibir cada registro em uma linha da tabela.
+                             </tr>";
 
-                            //  $linha: Cada linha da tabela cadas é armazenada como um array associativo
-
-                            $id = $linha['id'];//$id, $nome, $email: Extrai os valores das colunas id, nome e email.
-                            $nome = $linha['nome'];//guarda os dados puxados nessas varíaveis
-                            $email = $linha['email'];//guarda os dados puxados nessas varíaveis
-                            //.echo: Imprime uma linha da tabela com os valores extraídos e botões de ação (Editar e Excluir).
-                            echo "<tr> 
-        <td>$nome</td>
-        <td>$email</td>
-        <td width=150px>
-        <a href='editcad.php? id=$id' class='btn btn-success btn-sm'>Editar</a>
-          <a href='#' class='btn btn-danger btn-sm' >Excluir</a>
-          </td>
-        </tr>";
-                        }
-                        ?>
-                    </tbody>
-
-
-
-                </table>
-
-
+                             }
+                            ?>
+                        </tbody>
+                    </table>
             </div>
+
         </div>
     </div>
+<!-- Modal -->
+<div class="modal fade" id="confirma" tabindex="-1" role="dialog"
+ aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">confirmado a exclusao</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+        <button type="button" class="btn btn-primary">Sim</button>
+      </div>
+    </div>
+  </div>
+</div>
 
+   
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+ integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+ integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
